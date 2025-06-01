9430e7719b6d0b691d976afe98e69b779eebff7f
KaVin S <kavinadithya3@gmail.com>
2025-06-01 07:37:14 +0530

    Repository Created Learn Spring RestAPI

---
+++ src/test/java/com/techcrack/restful_web_service/RestFulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcrack.restful_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
